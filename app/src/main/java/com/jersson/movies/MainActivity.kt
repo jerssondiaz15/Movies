5bcfb802027bb99bd90ebb128003b4c21fecc848
Jersson Melanio Diaz Santa Cruz <68821837+jerssondiaz15@users.noreply.github.com>
2023-06-24 20:32:34 -0500

    modify

--- app/src/main/java/com/jersson/movies/MainActivity.kt
+++ app/src/main/java/com/jersson/movies/MainActivity.kt
@@ -18,12 +18,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             MoviesTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
     }
